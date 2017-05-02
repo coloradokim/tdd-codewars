9acedfd019f109267398a57816c6d554bc33000b
coloradokim <schlesinger.kim@gmail.com>
2017-05-02 11:45:16 -0600

    add disemvowel challenge

--- src/high_and_low.js
+++ src/high_and_low.js
@@ -1,14 +1,16 @@
 let findHighandLow = (stringOfNumbers) => {
     let arr = [];
     let result = {};
+
     stringOfNumbers = stringOfNumbers.split(" ");
 
     stringOfNumbers.forEach((string) => {
         arr.push(parseInt(string))
       });
-      
+
       result.high = Math.max(...arr)
       result.low = Math.min(...arr)
+
       return `${result.high} ${result.low}`;
     }
 
